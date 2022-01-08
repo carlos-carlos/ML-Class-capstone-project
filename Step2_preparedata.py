8917360123e3725c0ac64bb7d8602b6ad88dbd0f
carlos-carlos <carlh5462@fastmail.com>
2022-01-08 13:40:02 -0500

    - Changes script to only update and prepare that data

--- Step2_preparedata.py
+++ Step2_preparedata.py
@@ -14,20 +14,135 @@ import csv
 from pprint import pprint
 
 # GLOBAL SETTINGS
+# Time controls
+timezone = 'US/Eastern'
+current_timezone = pytz.timezone(timezone)
+
+now = datetime.datetime.now()  # current date and time
+today = datetime.datetime.combine(now, datetime.datetime.min.time())  # current data, midnight this morning UTC
+now = now.strftime('%s')  # make this epoch time but after you used it for the above purpose
+
+# Start and end times for API call in epoch
+#start = today - datetime.timedelta # would make it midnight yesterday morning
+start = today
+start = start.strftime('%s')  # make it epoch for the API
+start = int(start) - 3600  # Temporary solution to a timezone issue likely realted to EDT/EST change
+
+end = today - datetime.timedelta(90)  # get exactly 90 days worth of hourly data
+end = end.strftime('%s')  # make it epoch for the API
+
+# Fiat currency
+fiat ='usd'
 
 # Data directories
-coin_dataDir = 'DATA/COINHISTDATA/'
-#coin_dataDir = 'DATA/TESTDIR/' # Debug dir for testing I/O logic and/or issues. It should be a clone of the above dir.
-coinMDF_dataDir = 'DATA/COMBINEDDATA/'
-
-# Date ranges
-START = 2020
-END = 2022
+#coin_dataDir = 'DATA/COINHISTDATA/'
+coin_dataDir = 'DATA/TESTDIR/HIST/' # Debug dir for testing I/O logic and/or issues. It should be a clone of the above dir.
+#coinMDF_dataDir = 'DATA/COMBINEDDATA/'
+coinMDF_dataDir = 'DATA/TESTDIR/POOL/' # Debug dir for testing. It should be a clone of the above dir.
 
 # Helpers
 idx = pd.IndexSlice
 
 # END GLOBAL SETTINGS
+
+
+# Updater function
+def update_data(coin,fiat,start,end):
+
+    # API call for minutely data for the present day
+    try:
+        # API call, max 90 days for hourly prices. More than that its daily. API limitation
+        response = requests.get(
+            f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart/range?vs_currency={fiat}&from={end}&to={start}")
+        data = response.json()
+
+    except:
+        print("Couldn't connect to coingecko. Waiting 30 secs and trying again.....")
+        time.sleep(30)
+        # API call, max 90 days for hourly prices. More than that its daily. API limitation
+        response = requests.get(
+            f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart/range?vs_currency={fiat}&from={end}&to={start}")
+        data = response.json()
+
+
+    # Prepare prices as we did before but this time its minutes of the present day, not the hours of the prior 90 days
+    new_df = pd.DataFrame(data['prices'], columns=['dates', 'prices'])
+    new_df['dates'] = pd.to_datetime(new_df['dates'], unit='ms')
+    new_df = new_df.set_index('dates')
+    new_df.index = new_df.index.tz_localize('UTC').tz_convert(current_timezone)
+
+    # Prepare volume as we did before but this time its minutes of the present day, not the hours of the prior 90 days
+    volumes_df = pd.DataFrame(data['total_volumes'], columns=['dates', 'volumes'])
+    volumes_df['dates'] = pd.to_datetime(volumes_df['dates'], unit='ms')
+    volumes_df = volumes_df.set_index('dates')
+    volumes_df.index = volumes_df.index.tz_localize('UTC').tz_convert(current_timezone)
+
+    # Prepare hourly market cap data
+    cap_df = pd.DataFrame(data['market_caps'], columns=['dates', 'mcaps'])
+    cap_df['dates'] = pd.to_datetime(cap_df['dates'], unit='ms')
+    cap_df = cap_df.set_index('dates')
+    cap_df.index = cap_df.index.tz_localize('UTC').tz_convert(current_timezone)
+
+    # Calculate todays Open High Low Close Volume(OHLCV) values
+    min_df = new_df.groupby([new_df.index.date]).min()  # Get the lows for each day
+    max_df = new_df.groupby([new_df.index.date]).max()  # Get the highs for each day
+    open_df = new_df.groupby([new_df.index.date]).first()  # Get the first value of each day for Open
+    close_df = new_df.groupby([new_df.index.date]).last()  # Get the last value of each day for Close
+    volume_df = volumes_df.groupby([volumes_df.index.date]).last()  # Get the latest volume for the day
+    mcaps_df = cap_df.groupby([cap_df.index.date]).mean()  # Get the mean mcaps for each day
+
+    todays_ohlc_df = pd.concat(
+        [open_df['prices'], max_df['prices'], min_df['prices'], close_df['prices'], volume_df['volumes'],
+         mcaps_df['mcaps']], \
+        axis=1, keys=['Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap'])
+
+    # Debug code
+    #print(todays_ohlc_df.to_string())
+    #print(todays_ohlc_df.info)
+
+    return todays_ohlc_df
+
+# Decide each time whether to update the data or use the existing local data
+# Might want to do this if offline or if you are debugging and don't want to slam the API each time you run the script.
+print('Would you like to update the local data?')
+should_update = input('Enter "y" if YES. Enter anything else or nothing if NO').lower()
+
+if should_update == 'y':
+    # Get and prepare the existing data for the update
+    dir_list = os.listdir(coin_dataDir)
+    coins = [x.split('.')[0] for x in dir_list]
+
+    # Update the data
+    for coin in coins:
+
+        # Read in current data
+        current_df = pd.read_csv(coin_dataDir + f'{coin}.csv')
+        print(current_df.columns)
+        current_df.rename(columns={'Unnamed: 0': 'dates'}, inplace=True)
+        print(current_df.columns)
+        current_df = current_df.set_index('dates')
+        current_df.drop(current_df.index[-1],inplace=True)
+        current_df['dates'] = current_df.index
+
+        # Get the the new days data
+        update_df = update_data(coin,fiat,start,end)
+        update_df["dates"] = update_df.index
+
+        # Combine the 2 dataframes
+        new_df = pd.concat([current_df, update_df])
+        new_df['dates'] = pd.to_datetime(new_df['dates'])
+        new_df = new_df.drop_duplicates(subset=["dates"])
+        new_df = new_df.drop(['dates'], axis=1)
+
+        # Debug code
+        #print('THE DATA AFTER THE UPDATE')
+        #print(new_df.to_string())
+        #print(new_df.info())
+
+        # Save the updated
+        new_df.to_csv(f'{coin_dataDir}{coin}.csv')
+        print(f"Data for {coin} saved to {coin_dataDir}")
+
 
 # Read in the data csvs into Pandas
 dir_list = os.listdir(coin_dataDir)
@@ -53,7 +168,7 @@ coin_mdf = pd.concat(coin_dfs)
 
 # Sort index
 coin_mdf.sort_index(inplace=True)
-#print(coin_mdf.to_string())
+print(coin_mdf.to_string())
 #print(coin_mdf.loc[('2022-01-02', 'bitcoin')]['Close'])
 #print(coin_mdf.index)
 
@@ -64,86 +179,13 @@ isdir = os.path.isdir(dataDir)
 
 # Save the initial pool MDF in a seprate directory for persistence and "just in case" purposes
 if isdir == False:
-    os.makedirs(dataDir)
-    print("Directory '% s' created" % dataDir)
-    coin_mdf.to_csv(f'{dataDir}CoinPool.csv')
-    print(f"The initial pool of coins has been saved to {dataDir} as a MultiIndex dataframe")
+    os.makedirs(coinMDF_dataDir)
+    print("Directory '% s' created" % coinMDF_dataDir)
+    coin_mdf.to_csv(f'{coinMDF_dataDir}CoinPool.csv')
+    print(f"The initial pool of coins has been saved to {coinMDF_dataDir} as a MultiIndex dataframe")
 
 else:
-    coin_mdf.to_csv(f'{dataDir}CoinPool.csv')
-    print(f"The initial pool of coins has been saved to {dataDir} as a MultiIndex dataframe")
+    coin_mdf.to_csv(f'{coinMDF_dataDir}CoinPool.csv')
+    print(f"The initial pool of coins has been saved to {coinMDF_dataDir} as a MultiIndex dataframe")
 
-
-# Isolate the close prices
-close_df = coin_mdf.loc[idx[str(START):str(END), :], 'Close'].unstack('Coin')
-#print(close_df.info)
-
-# Calculate lagged returns
-outlier_threshold = 0.01
-data = pd.DataFrame()
-lags = [1, 2, 3, 6, 9, 12]
-
-# This block stacks the wide MDF to long formant while also:
-# Winsorizing outliers in the returns at the 1% and 99% levels
-# Capping outliers and the aforementioned levels
-# Normalize the returns via geometric mean
-for lag in lags:
-    data[f'return_{lag}m'] = (close_df
-                           .pct_change(lag)
-                           .stack()
-                           .pipe(lambda x: x.clip(lower=x.quantile(outlier_threshold),
-                                                  upper=x.quantile(1-outlier_threshold)))
-                           .add(1)
-                           .pow(1/lag)
-                           .sub(1)
-                           )
-# Resulting in compunded daily returns for the six monthly periods in the lags list above
-data = data.swaplevel().dropna()
-#print(data.info())
-#print(data.to_string())
-
-# Drop coins with less than one year of returns
-min_obs = 365
-nobs = data.groupby(level='Coin').size()
-keep = nobs[nobs>min_obs].index
-data = data.loc[idx[keep,:], :]
-#print(data.info())
-
-
-# Cluster map with Seaborn
-clusterMap = sns.clustermap(data.corr('spearman'), annot=True, center=0, cmap='Blues')
-clusterMap.savefig('Cluster_Spearman_Blue.png')
-
-print(data.index.get_level_values('Coin').nunique())
-
-# Compute momentum factor based on difference between 3 and 12 month returns
-# And for differences between all periods and the most recent month returns
-
-# Most recent month and the rest
-for lag in [2,3,6,9,12]:
-    data[f'momentum_{lag}'] = data[f'return_{lag}m'].sub(data.return_1m)
-
-# 3 and 12 month returns
-data["momentum_3_12"] = data["return_12m"].sub(data.return_3m)
-
-# Move historical returns up to current period so they can be used as features
-for t in range(1,7):
-    data[f'return_1m_t-{t}'] = data.groupby(level='Coin').return_1m.shift(t)
-
-# Returns for various holding periods, using the previous normalized returns, shifted back to align with current features
-for t in [1,2,3,6,12]:
-    data[f'target_{t}m'] = (data.groupby(level='Coin')
-                            [f'return_{t}m'].shift(-t))
-
-print(data.info())
-print(data.describe())
-#print(data.to_string())
-
-
-# Adding time indicators
-dates = data.index.get_level_values('Dates')
-data['year'] = dates.year
-data['month'] = dates.month
-
-#print(dates)
 
