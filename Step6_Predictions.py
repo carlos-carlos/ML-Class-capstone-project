e287aeb1f1caae0422f857c729bcea79902bc1ce
carlos-carlos <carlh5462@fastmail.com>
2022-01-25 21:42:09 -0500

    - save progress

--- Step6_Predictions.py
+++ Step6_Predictions.py
@@ -21,6 +21,8 @@ from matplotlib.ticker import FuncFormatter
 model_dataDir = 'DATA/MODELDATA/'
 plot_dataDir = 'DATA/INITIAL_INSIGHTS/MOMENTUM_FACTORS/STATINFER/'
 
+# Time periods settings
+YEAR = 365
 
 # Helpers
 sns.set_style('darkgrid')
@@ -43,4 +45,89 @@ data = (model_mdf
 
 data = data.drop([c for c in data.columns if 'lag' in c], axis=1)
 
-print(data.info(null_counts=True))
+#print(data.info(null_counts=True))
+
+
+y = data.filter(like='target')
+X = data.drop(y.columns, axis=1)
+#print(X.info(null_counts=True))
+#print(y.info(null_counts=True))
+
+# This is a custom implementation of TimeSeriesSplit CV from sklearn for finance data.
+# It is taken from code developed by Stefen Jansen and Marcos Lopez de Prado.
+# It is meant to eliminate any spill over between several train/test pairs derived from the dataset.
+# Financial data is especially suseptible to such leakage and so a custom TimeSeries CV approach is necessary.
+# The class below creates several train/test split pairs from the dataset and spearates them using
+# purging, embargoing, and Combinatorial CV techniques.
+
+class MultipleTimeSeriesCV:
+    """Generates tuples of train_idx, test_idx pairs
+    Assumes the MultiIndex contains levels 'symbol' and 'date'
+    purges overlapping outcomes"""
+
+    def __init__(self,
+                 n_splits=3,
+                 train_period_length=126,
+                 test_period_length=21,
+                 lookahead=None,
+                 shuffle=False):
+        self.n_splits = n_splits
+        self.lookahead = lookahead
+        self.test_length = test_period_length
+        self.train_length = train_period_length
+        self.shuffle = shuffle
+
+    def split(self, X, y=None, groups=None):
+        unique_dates = X.index.get_level_values('Dates').unique()
+        days = sorted(unique_dates, reverse=True)
+
+        split_idx = []
+        for i in range(self.n_splits):
+            test_end_idx = i * self.test_length
+            test_start_idx = test_end_idx + self.test_length
+            train_end_idx = test_start_idx + + self.lookahead - 1
+            train_start_idx = train_end_idx + self.train_length + self.lookahead - 1
+            split_idx.append([train_start_idx, train_end_idx,
+                              test_start_idx, test_end_idx])
+
+        dates = X.reset_index()[['Dates']]
+        for train_start, train_end, test_start, test_end in split_idx:
+            train_idx = dates[(dates.Dates > days[train_start])
+                              & (dates.Dates <= days[train_end])].index
+            test_idx = dates[(dates.Dates > days[test_start])
+                             & (dates.Dates <= days[test_end])].index
+            if self.shuffle:
+                np.random.shuffle(list(train_idx))
+            yield train_idx, test_idx
+
+    def get_n_splits(self, X, y, groups=None):
+        return self.n_splits
+
+# Set the periods of time for training, testing, and the total base data periods for the pairs.
+train_period_length = 60
+test_period_length = 7
+n_splits = int(2 * YEAR/test_period_length)
+lookahead = 1
+
+cv = MultipleTimeSeriesCV(n_splits=n_splits,
+                          test_period_length=test_period_length,
+                          lookahead=lookahead,
+                          train_period_length=train_period_length)
+
+# Check if it worked
+i = 0
+for train_idx, test_idx in cv.split(X=data):
+    train = data.iloc[train_idx]
+    train_dates = train.index.get_level_values('Dates')
+    test = data.iloc[test_idx]
+    test_dates = test.index.get_level_values('Dates')
+    df = train.reset_index().append(test.reset_index())
+    n = len(df)
+    assert n== len(df.drop_duplicates())
+    print(train.groupby(level='Coin').size().value_counts().index[0],
+          train_dates.min().date(), train_dates.max().date(),
+          test.groupby(level='Coin').size().value_counts().index[0],
+          test_dates.min().date(), test_dates.max().date())
+    i += 1
+    if i == 10:
+        break
